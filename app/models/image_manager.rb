96f3cd45f9460007853053d31420bcb8d60ec7d2
Paul Cockrell <paulcockrell@gmail.com>
2014-09-06 22:29:06 +0100

    Introduce threading and remove redundant methods that have been moved to the image class

--- app/models/image_manager.rb
+++ app/models/image_manager.rb
@@ -3,15 +3,16 @@ class ImageManager
   def self.find(registration, stock_reference)
     image_array = create_image_instances(registration, stock_reference)
 
-    begin
-      image_array.map do |image|
-        RestClient.get(image.url) do |response, request, result, &block|
+    image_array.map do |image|
+      Thread.new do
+        begin
+          response = RestClient.get(image.url)
           image.present = response.code == 200 ? true : false
+        rescue SocketError
+          image.present = false
         end
       end
-    rescue Exception => e
-      # some stuff here
-    end
+    end.each { |thread| thread.join }
 
     image_array
   end
@@ -20,9 +21,6 @@ class ImageManager
   private
 
   def self.create_image_instances(registration, stock_reference)
-    registration = registration.strip.upcase
-    stock_reference = stock_reference.strip.upcase
-
     [].tap do |el|
       Image::IMAGE_SIZES.each do |image_size|
         Image::CAMERA_ANGLES.each do |camera_angle|
@@ -30,15 +28,11 @@ class ImageManager
             :camera_angle => camera_angle,
             :image_size => image_size,
             :present => false,
-            :registration => registration,
-            :stock_reference => stock_reference
+            :registration => registration.strip.upcase,
+            :stock_reference => stock_reference.strip.upcase
           )
         end
       end
     end
-  end
-
-  def self.obfuscate(registration, stock_reference)
-    Obfuscator::RegistrationReversor.obfuscate(registration.to_s, stock_reference.to_s)
   end
 end
