d15d45a270e1f2e8d2368194e1faee2932aa713e
Paul Cockrell <paulcockrell@gmail.com>
2014-09-08 11:58:25 +0100

    Return the execption in the JSON response for 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::API
   private
 
   def server_error(exception)
-    render :json => {:success => false}, :status => 404
+    render :json => {:success => false, :error => exception.inspect}, :status => 404
   end
 end
